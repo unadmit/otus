622c800490096163e325fb24d897333aa2da871c
una dmit <oldboa@yandex.ru>
2023-01-25 03:42:07 +0700

    modified sh

--- prepare.sh
+++ prepare.sh
@@ -13,7 +13,7 @@ then
     iptables-restore < ./otus-linux-basic/$target/iptables
     iptables-save
     service iptables save
-    rsync -vrp -e "ssh -i $HOME/.ssh/id_rsa" root@192.168.1.100:/root/otus-linux-basic/$target /root/otus-linux-basic 
+    rsync -vrp -e "ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no" root@192.168.1.200:/root/otus-linux-basic/$target /root/otus-linux-basic 
     yes | cp -rf ./otus-linux-basic/$target/ifcfg-enp0s3 /etc/sysconfig/network-scripts/ifcfg-enp0s3
     systemctl restart network
     hostnamectl set-hostname $target
