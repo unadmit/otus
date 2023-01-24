ec68071768fc757bf060a17e1275005b49ed0f86
una dmit <oldboa@yandex.ru>
2023-01-25 02:00:29 +0700

    change bat

--- create-vm.bat
+++ create-vm.bat
@@ -10,5 +10,5 @@ for %%x in (
     )
 C:\Progra~1\Oracle\VirtualBox\VBoxManage.exe modifyvm "elk" --memory 4096
 C:\Progra~1\Oracle\VirtualBox\VBoxManage.exe startvm "backup"
-C:\Progra~1\PuTTY\psftp root@192.168.1.100 -b c:\Temp\otus-linux-basic\psftp-commands.txt
+C:\Progra~1\PuTTY\psftp root@192.168.1.100 -i c:\temp\id_rsa.ppk -b c:\Temp\otus-linux-basic\psftp-commands.txt
 C:\Progra~1\PuTTY\putty.exe 192.168.1.100 -ssh -l root -i c:\temp\id_rsa.ppk
