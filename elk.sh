28cbef147fe92b4931a59d48a506847ff3b1bc0c
una dmit <oldboa@yandex.ru>
2023-01-25 03:02:51 +0700

    modified sh

--- elk.sh
+++ elk.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 yum -y install rsync
-yes | rsync -zvra root@192.168.1.200:/root/data /root
 yum -y install java-openjdk-devel java-openjdk
-rpm -i ./data/*.rpm
-cp -v ./data/jvm.options /etc/elasticsearch/jvm.options.d/
+rpm -i ./otus-linux-basic/elk/*.rpm
+cp -v ./otus-linux-basic/elk/jvm.options /etc/elasticsearch/jvm.options.d/
 systemctl enable --now elasticsearch.service
 curl -X PUT "http://127.0.0.1:9200/mytest_index"
-yes | cp -vf ./data/kibana.yml /etc/kibana/
+yes | cp -vf ./otus-linux-basic/elk/kibana.yml /etc/kibana/
 systemctl enable --now kibana
-yes | cp -vf ./data/logstash.yml /etc/logstash/
-cp -v ./data/logstash-nginx-es.conf /etc/logstash/conf.d/
+yes | cp -vf ./otus-linux-basic/elk/logstash.yml /etc/logstash/
+cp -v ./otus-linux-basic/elk/logstash-nginx-es.conf /etc/logstash/conf.d/
 systemctl enable --now logstash.service
-yes | cp -vf ./data/filebeat.yml /etc/filebeat/
+yes | cp -vf ./otus-linux-basic/elk/filebeat.yml /etc/filebeat/
 systemctl enable --now filebeat
--- prometheus.sh
+++ prometheus.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-curl -LO "https://github.com/prometheus/prometheus/releases/download/v2.41.0/prometheus-2.41.0.linux-amd64.tar.gz"
-curl -LO "https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz"
-tar -xvf prometheus-2.41.0.linux-amd64.tar.gz
-tar -xvf node_exporter-1.5.0.linux-amd64.tar.gz
-curl -LO "https://dl.grafana.com/oss/release/grafana-9.3.2-1.x86_64.rpm"
+tar -xvf ./otus-linux-basic/prometheus/prometheus-2.41.0.linux-amd64.tar.gz
+tar -xvf ./otus-linux-basic/prometheus/node_exporter-1.5.0.linux-amd64.tar.gz
 useradd --no-create-home --shell /usr/sbin/nologin prometheus
 useradd --no-create-home --shell /bin/false node_exporter
 mkdir {/etc,/var/lib}/prometheus
@@ -13,13 +10,13 @@ rsync -P ./prometheus-2.41.0.linux-amd64/prometheus ./prometheus-2.41.0.linux-am
 chown -v prometheus: /usr/local/sbin/prometheus
 chown -v prometheus: /usr/local/sbin/promtool
 chown node_exporter: /usr/local/sbin/node_exporter
-cp -v ./node_exporter.service /etc/systemd/system
-cp -v ./prometheus.service /etc/systemd/system
-cp -v ./prometheus.yml /etc/prometheus/
+cp -v ./otus-linux-basic/prometheus/node_exporter.service /etc/systemd/system
+cp -v ./otus-linux-basic/prometheus/prometheus.service /etc/systemd/system
+cp -v ./otus-linux-basic/prometheus/prometheus.yml /etc/prometheus/
 cp -v -r ./prometheus-2.41.0.linux-amd64/console{_libraries,s}/ /etc/prometheus/
 chown -v prometheus: /etc/prometheus
 systemctl daemon-reload
 systemctl enable --now prometheus.service
 systemctl enable --now node_exporter
-yum -y install ./grafana-9.3.2-1.x86_64.rpm
+yum -y install ./otus-linux-basic/prometheus/grafana-9.3.2-1.x86_64.rpm
 systemctl enable --now grafana-server.service
